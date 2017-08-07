48780eea441fcb23498a8408b7ddf0874641e7ec
Louie Rosero <rosero.louie@yahoo.com>
2017-08-07 23:45:22 +0800

    Commit 2
    Waterwheel Image Carousel is now working.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-$(document).ready(function() {
-	var carousel = $("#carousel").waterwheelCarousel({
-		flankingItems: 3
-	});
-	$("#prev").bind('click', function() {
-		carousel.prev();
-		return false;
-	});
-	$("#next").bind('click', function() {
-		carousel.next();
-		return false;
-	});
+$(document).ready(function () {
+    var carousel = $("#carousel").waterwheelCarousel({
+      flankingItems: 3
+    });
+
+    $('#prev').bind('click', function () {
+      carousel.prev();
+      return false
+    });
+
+    $('#next').bind('click', function () {
+      carousel.next();
+      return false;
+    });
+	
 });
